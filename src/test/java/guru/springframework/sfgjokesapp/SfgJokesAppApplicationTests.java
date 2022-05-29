e5afb2d5af84037e42007edbe2ad76bedcf5c4d5
hnnfr <59726038+hnnfr@users.noreply.github.com>
2022-05-29 08:19:38 +0200

    Create a Spring Boot App - Close #1

---
+++ src/test/java/guru/springframework/sfgjokesapp/SfgJokesAppApplicationTests.java
@@ -0,0 +1,16 @@
+package guru.springframework.sfgjokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class SfgJokesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true, "Load the context");
+    }
+
+}
