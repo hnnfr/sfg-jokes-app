b8917d028572bab7aa983cd9d0a1965979f2835c
hnnfr <59726038+hnnfr@users.noreply.github.com>
2022-05-29 08:36:23 +0200

    Add Joke Service - Close #3

---
+++ src/main/java/guru/springframework/sfgjokesapp/services/JokesServiceImpl.java
@@ -0,0 +1,15 @@
+package guru.springframework.sfgjokesapp.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokesServiceImpl implements JokesService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes = new ChuckNorrisQuotes();
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
